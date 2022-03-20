b3e6d9789131f887648873144fb78bd3b75ea871
Amanda Nobre <34144662+AmandaNobre@users.noreply.github.com>
2022-03-20 15:10:39 -0300

    Colocando banners

---
+++ src/components/CardIcon/index.tsx
@@ -0,0 +1,12 @@
+import "./styles.scss";
+
+const CardIcon = () => {
+  return (
+    <div className="principalCardIcon">
+      <i className="fa fa-glasses" />
+      <span>Geek</span>
+    </div>
+  );
+};
+
+export default CardIcon;
